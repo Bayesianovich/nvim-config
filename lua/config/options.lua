daa88c9a6e9dc8c054271997ad1fc83a57fedaea
root <root@DESKTOP-03DHTVL.localdomain>
2026-03-29 11:54:44 +0800

    Refine indentation defaults by filetype

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,12 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true
-
-vim.opt.formatoptions = "jqlnt" -- 移除 c, r, o 等自动注释选项
+vim.opt.timeoutlen = 500
 
 local function set_cyan_cursor()
   local cursor_bg = "#00ffff"
